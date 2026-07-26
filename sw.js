c593b0ecd25cf3c3293535b14925c193fe06b3bf
qiqimimi1002 <434364435@qq.com>
2026-07-26 10:42:14 +0800

    刷新V3.0手机缓存

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nanny-salary-v23-mobile-20260724';
+const CACHE='nanny-salary-v30-20260726';
 const ASSETS=['./','./index.html','./style.css','./app-core.js','./app-ui.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
